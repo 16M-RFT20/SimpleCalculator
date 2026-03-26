118634b3d887b1159ca49f30e2355507061ae64e
kim dg <dgkim729@gmail.com>
2026-03-26 09:56:49 +0900

    UI 배치 조정

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,330 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            SimpleCalculator = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            CE = new Button();
+            C = new Button();
+            del = new Button();
+            divide = new Button();
+            button７ = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            multiply = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            minus = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            plus = new Button();
+            negate = new Button();
+            button0 = new Button();
+            DecimalPoint = new Button();
+            equal = new Button();
+            SuspendLayout();
+            // 
+            // SimpleCalculator
+            // 
+            SimpleCalculator.AutoSize = true;
+            SimpleCalculator.Font = new Font("한컴 고딕", 30F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            SimpleCalculator.ForeColor = Color.Blue;
+            SimpleCalculator.Location = new Point(50, 23);
+            SimpleCalculator.Name = "SimpleCalculator";
+            SimpleCalculator.Size = new Size(430, 65);
+            SimpleCalculator.TabIndex = 0;
+            SimpleCalculator.Text = "Simple Calculator";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            textBox1.Location = new Point(83, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(338, 43);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            textBox2.Location = new Point(83, 154);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(338, 43);
+            textBox2.TabIndex = 2;
+            // 
+            // CE
+            // 
+            CE.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            CE.Location = new Point(83, 216);
+            CE.Name = "CE";
+            CE.Size = new Size(80, 44);
+            CE.TabIndex = 3;
+            CE.Text = "CE";
+            CE.UseVisualStyleBackColor = true;
+            // 
+            // C
+            // 
+            C.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            C.Location = new Point(169, 216);
+            C.Name = "C";
+            C.Size = new Size(80, 44);
+            C.TabIndex = 4;
+            C.Text = "C";
+            C.UseVisualStyleBackColor = true;
+            // 
+            // del
+            // 
+            del.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            del.Location = new Point(255, 216);
+            del.Name = "del";
+            del.Size = new Size(80, 44);
+            del.TabIndex = 5;
+            del.Text = "del";
+            del.UseVisualStyleBackColor = true;
+            // 
+            // divide
+            // 
+            divide.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            divide.ForeColor = Color.Red;
+            divide.Location = new Point(341, 216);
+            divide.Name = "divide";
+            divide.Size = new Size(80, 44);
+            divide.TabIndex = 6;
+            divide.Text = "÷";
+            divide.UseVisualStyleBackColor = true;
+            // 
+            // button７
+            // 
+            button７.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button７.ForeColor = Color.Blue;
+            button７.Location = new Point(83, 266);
+            button７.Name = "button７";
+            button７.Size = new Size(80, 44);
+            button７.TabIndex = 7;
+            button７.Text = "７";
+            button７.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button8.ForeColor = Color.Blue;
+            button8.Location = new Point(169, 266);
+            button8.Name = "button8";
+            button8.Size = new Size(80, 44);
+            button8.TabIndex = 8;
+            button8.Text = "８";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button9.ForeColor = Color.Blue;
+            button9.Location = new Point(255, 266);
+            button9.Name = "button9";
+            button9.Size = new Size(80, 44);
+            button9.TabIndex = 9;
+            button9.Text = "９";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // multiply
+            // 
+            multiply.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            multiply.ForeColor = Color.Red;
+            multiply.Location = new Point(341, 266);
+            multiply.Name = "multiply";
+            multiply.Size = new Size(80, 44);
+            multiply.TabIndex = 10;
+            multiply.Text = "x";
+            multiply.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button4.ForeColor = Color.Blue;
+            button4.Location = new Point(83, 316);
+            button4.Name = "button4";
+            button4.Size = new Size(80, 44);
+            button4.TabIndex = 11;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button5.ForeColor = Color.Blue;
+            button5.Location = new Point(169, 316);
+            button5.Name = "button5";
+            button5.Size = new Size(80, 44);
+            button5.TabIndex = 12;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button6.ForeColor = Color.Blue;
+            button6.Location = new Point(255, 316);
+            button6.Name = "button6";
+            button6.Size = new Size(80, 44);
+            button6.TabIndex = 13;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // minus
+            // 
+            minus.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            minus.ForeColor = Color.Red;
+            minus.Location = new Point(341, 316);
+            minus.Name = "minus";
+            minus.Size = new Size(80, 44);
+            minus.TabIndex = 14;
+            minus.Text = "-";
+            minus.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.ForeColor = Color.Blue;
+            button1.Location = new Point(83, 366);
+            button1.Name = "button1";
+            button1.Size = new Size(80, 44);
+            button1.TabIndex = 15;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button2.ForeColor = Color.Blue;
+            button2.Location = new Point(169, 366);
+            button2.Name = "button2";
+            button2.Size = new Size(80, 44);
+            button2.TabIndex = 16;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button3.ForeColor = Color.Blue;
+            button3.Location = new Point(255, 366);
+            button3.Name = "button3";
+            button3.Size = new Size(80, 44);
+            button3.TabIndex = 17;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // plus
+            // 
+            plus.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            plus.ForeColor = Color.Red;
+            plus.Location = new Point(341, 366);
+            plus.Name = "plus";
+            plus.Size = new Size(80, 44);
+            plus.TabIndex = 18;
+            plus.Text = "+";
+            plus.UseVisualStyleBackColor = true;
+            // 
+            // negate
+            // 
+            negate.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            negate.Location = new Point(83, 416);
+            negate.Name = "negate";
+            negate.Size = new Size(80, 44);
+            negate.TabIndex = 19;
+            negate.Text = "+/-";
+            negate.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            button0.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button0.ForeColor = Color.Blue;
+            button0.Location = new Point(169, 416);
+            button0.Name = "button0";
+            button0.Size = new Size(80, 44);
+            button0.TabIndex = 20;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            // 
+            // DecimalPoint
+            // 
+            DecimalPoint.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            DecimalPoint.Location = new Point(255, 416);
+            DecimalPoint.Name = "DecimalPoint";
+            DecimalPoint.Size = new Size(80, 44);
+            DecimalPoint.TabIndex = 21;
+            DecimalPoint.Text = ".";
+            DecimalPoint.UseVisualStyleBackColor = true;
+            // 
+            // equal
+            // 
+            equal.Font = new Font("한컴 고딕", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            equal.Location = new Point(341, 416);
+            equal.Name = "equal";
+            equal.Size = new Size(80, 44);
+            equal.TabIndex = 22;
+            equal.Text = "=";
+            equal.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(519, 526);
+            Controls.Add(equal);
+            Controls.Add(DecimalPoint);
+            Controls.Add(button0);
+            Controls.Add(negate);
+            Controls.Add(plus);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(minus);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(multiply);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button７);
+            Controls.Add(divide);
+            Controls.Add(del);
+            Controls.Add(C);
+            Controls.Add(CE);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(SimpleCalculator);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label SimpleCalculator;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button CE;
+        private Button C;
+        private Button del;
+        private Button divide;
+        private Button button７;
+        private Button button8;
+        private Button button9;
+        private Button multiply;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button minus;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button plus;
+        private Button negate;
+        private Button button0;
+        private Button DecimalPoint;
+        private Button equal;
     }
 }
